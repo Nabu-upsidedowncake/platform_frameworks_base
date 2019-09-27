97cfdd9b540f71cb2f2a554b564fead542a87dc8
maxwen <max.weninger@gmail.com>
2019-09-27 23:13:28 +0200

    base: Add CPU info overlay
    includes all modifications and fixes from P
    
    @neobuddy89: Remove redundant DreamManager code
    
    Change-Id: I0e6dee53ebcd864a522cfd3d78a5abb253991a9c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    SystemUI: Add CPUInfo toggle tile
    
    Change-Id: I84fcdff1c29c0bcd3f85d60b4ccba25288ebbddd
    Co-authored-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/custom/QSModuleCustom.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/custom/QSModuleCustom.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
+import com.android.systemui.qs.tiles.CPUInfoTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.UsbTetherTile
@@ -77,6 +78,12 @@ interface QSModuleCustom {
     @IntoMap
     @StringKey(CompassTile.TILE_SPEC)
     fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
+
+    /** Inject CPUInfoTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CPUInfoTile.TILE_SPEC)
+    fun bindCPUInfoTile(cpuInfoTile: CPUInfoTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
