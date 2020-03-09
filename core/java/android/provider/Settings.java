5ea607ee1d35f7f7148c79939782cbcdf3693fa5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-09 11:10:34 -0300

    SystemUI: Allow enabling compact layout of navigation bar
    Includes also:
    
    commit 4a73c8ace697c7cb78dca4fe83b6a0b2bfc6bd2d
    Author: Alex <du.alexcruz@gmail.com>
    Date:   Fri Feb 28 04:12:52 2020 -0500
    
        Navbar: Ensure 2/3 button layout options play nice with gesture navbar
    
        The 2 and 3 button navbar layout options were causing the gesture 'pill' to
        disappear when enabled. This commit ensures that the pill survives when switching
        and keeps the user's layout for 2 and 3 button navbar in play when they return.
    
        Test:
        - Enable 2 or 3 button navbar
        - Go into layout options
        - Set the layout to compact or any other option but 'Normal'
        - Enable the gesture navbar
    
        Change-Id: Id4f0bc8c7c155090bb9f7ce24edd5bdbbd7553d5
    
    Change-Id: I68ecb01e7c8d74462be55bab9ad395dd7d3a2d90
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6037,6 +6037,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_MENU_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
 
         /**
+         * boolean value. toggles compact layout in navigation bar
+         * @hide
+         */
+        public static final String NAV_BAR_COMPACT_LAYOUT = "navigation_bar_compact_layout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6218,6 +6224,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_ENABLED);
             PRIVATE_SETTINGS.add(ADAPTIVE_PLAYBACK_TIMEOUT);
             PRIVATE_SETTINGS.add(VOLUME_KEY_CURSOR_CONTROL);
+            PRIVATE_SETTINGS.add(NAV_BAR_COMPACT_LAYOUT);
         }
 
         /**
