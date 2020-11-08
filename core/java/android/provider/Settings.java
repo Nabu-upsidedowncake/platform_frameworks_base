5823b1399367183a183e0b768e38fdc7baff20d2
ezio84 <brabus84@gmail.com>
2020-11-08 10:17:01 +0100

    Ambient music: pulse on new music tracks and Now Playing info [1/2]
    * Adapt to Android 12 (Jayant-Deshmukh)
    
    [spezi77] Adapt to TunerService & moved to Settings.Secure where other doze stuff is.
    
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I064b92afe59d1a611ef42593fdd8ef7f82fa847e
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11734,6 +11734,12 @@ public final class Settings {
          */
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
+       /**
+         * Whether to pulse ambient on new music tracks
+         * @hide
+         */
+        public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
+
         /**
          * Whether to use black theme for dark mode
          * @hide
