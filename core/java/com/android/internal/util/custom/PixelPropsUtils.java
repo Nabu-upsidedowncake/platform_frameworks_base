ded05850995b03a0c96eafee1741b48763e14465
chiteroman <98092901+chiteroman@users.noreply.github.com>
2023-11-30 01:51:26 +0900

    Let's stop this madness please
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -207,14 +207,13 @@ public class PixelPropsUtils {
     }
 
     private static void spoofBuildGms() {
-        // Alter build parameters to Nexus 5X for avoiding hardware attestation enforcement
-        setPropValue("PRODUCT", "foster_e");
-        setPropValue("MODEL", "SHIELD Android TV");
+        // Alter build parameters to avoid hardware attestation enforcement
+        setPropValue("BRAND", "NVIDIA");
+        setPropValue("MANUFACTURER", "NVIDIA");
         setPropValue("DEVICE", "foster");
         setPropValue("FINGERPRINT", "NVIDIA/foster_e/foster:7.0/NRD90M/2427173_1038.2788:user/release-keys");
-        setPropValue("TYPE", "user");
-        setPropValue("TAGS", "release-keys");
-        setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
+        setPropValue("MODEL", "SHIELD Android TV");
+        setPropValue("PRODUCT", "foster_e");
         setVersionFieldString("SECURITY_PATCH", "2018-01-05");
     }
 
