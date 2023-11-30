3a61414d1fef4d6443cb38859303e6f71b574618
saikiran2001 <bjsaikiran@gmail.com>
2023-11-30 11:40:15 +0000

    Revert "DSR: Fix DSR when we have toast window"
    This reverts commit 815cdccb85347aaff23a72993029c8e0742c736f.

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -766,8 +766,6 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     @Nullable
     private ActivityRecord mTracedResumedActivity;
 
-    boolean toastWindow = false;
-
     /** If non-null, we are tracking the time the user spends in the currently focused app. */
     AppTimeTracker mCurAppTimeTracker;
 
@@ -5273,18 +5271,6 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     CompatibilityInfo compatibilityInfoForPackageLocked(ApplicationInfo ai) {
         return mCompatModePackages.compatibilityInfoForPackageLocked(ai);
-    }
-
-    void setToastWindow() {
-        toastWindow = true;
-    }
-
-    void resetToastWindow() {
-        toastWindow = false;
-    }
-
-    boolean getToastWindow() {
-        return toastWindow;
     }
 
     /**
