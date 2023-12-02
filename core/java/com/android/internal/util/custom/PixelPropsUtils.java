20080b4ffd1ba55b94332890687bcfb7004fa99b
saikiran2001 <bjsaikiran@gmail.com>
2023-12-02 05:51:14 +0000

    Revert "services: Fix google apps permission denials"
    This reverts commit 466e974a35c7d6b513b0bca3b09f385d1955ce25.

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -328,9 +328,15 @@ public class PixelPropsUtils {
     public static boolean shouldBypassTaskPermission(Context context) {
         // GMS doesn't have MANAGE_ACTIVITY_TASKS permission
         final int callingUid = Binder.getCallingUid();
-        final String callingPackage = context.getPackageManager().getNameForUid(callingUid);
-        dlog("shouldBypassTaskPermission: callingPackage:" + callingPackage);
-        return callingPackage != null && callingPackage.toLowerCase().contains("google");
+        final int gmsUid;
+        try {
+            gmsUid = context.getPackageManager().getApplicationInfo(PACKAGE_GMS, 0).uid;
+            dlog("shouldBypassTaskPermission: gmsUid:" + gmsUid + " callingUid:" + callingUid);
+        } catch (Exception e) {
+            Log.e(TAG, "shouldBypassTaskPermission: unable to get gms uid", e);
+            return false;
+        }
+        return gmsUid == callingUid;
     }
 
     private static boolean isCallerSafetyNet() {
