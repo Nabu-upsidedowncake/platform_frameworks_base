LineageOS/android_frameworks_base

SystemUI: screenshot: open the screenshot instead of edit
Open the taken screenshot in gallery application when clicking on the
preview. There is a seperate edit button, so the preview imageview
should open gallery instead.

Test: m
Change-Id: I8f882ddd0da47ca50acc37d25ee0866ce5698e4f

packages/SystemUI/tests/src/com/android/systemui/screenshot/ScreenshotNotificationSmartActionsTest.java
@@ -169,6 +169,34 @@ public class ScreenshotNotificationSmartActionsTest extends SysuiTestCase {
         assertEquals(smartActions.size(), 0);
     }
 
+    // Tests for view action extras
+    @Test
+    public void testViewActionExtras() {
+        if (Looper.myLooper() == null) {
+            Looper.prepare();
+        }
+
+        ScreenshotController.SaveImageInBackgroundData
+                data = new ScreenshotController.SaveImageInBackgroundData();
+        data.image = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        data.finisher = null;
+        data.mActionsReadyListener = null;
+        SaveImageInBackgroundTask task =
+                new SaveImageInBackgroundTask(mContext, null, mScreenshotSmartActions, data,
+                        ActionTransition::new);
+
+        Notification.Action viewAction = task.createShareAction(mContext, mContext.getResources(),
+                Uri.parse("Screenshot_123.png")).get().action;
+
+        Intent intent = shareAction.actionIntent.getIntent();
+        assertNotNull(intent);
+        Bundle bundle = intent.getExtras();
+        assertTrue(bundle.containsKey(ScreenshotController.EXTRA_ID));
+        assertTrue(bundle.containsKey(ScreenshotController.EXTRA_SMART_ACTIONS_ENABLED));
+        assertEquals(ScreenshotController.ACTION_TYPE_VIEW, shareAction.title);
+        assertEquals(Intent.ACTION_SEND, intent.getAction());
+    }
+
     // Tests for share action extras
     @Test
     public void testShareActionExtras() {

Nabu-upsidedowncake/platform_frameworks_base

Test: atest com.android.systemui.screenshot.ScreenshotNotificationSmartActionsTest

packages/SystemUI/tests/src/com/android/systemui/screenshot/ScreenshotNotificationSmartActionsTest.java
@@ -167,6 +167,34 @@ public class ScreenshotNotificationSmartActionsTest extends SysuiTestCase {
         assertEquals(smartActions.size(), 0);
     }
 
+    // Tests for view action extras
+    @Test
+    public void testViewActionExtras() {
+        if (Looper.myLooper() == null) {
+            Looper.prepare();
+        }
+
+        ScreenshotController.SaveImageInBackgroundData
+                data = new ScreenshotController.SaveImageInBackgroundData();
+        data.image = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        data.finisher = null;
+        data.mActionsReadyListener = null;
+        SaveImageInBackgroundTask task =
+                new SaveImageInBackgroundTask(mContext, null, mScreenshotSmartActions, data,
+                        ActionTransition::new);
+
+        Notification.Action viewAction = task.createViewAction(mContext, mContext.getResources(),
+                Uri.parse("Screenshot_123.png")).get().action;
+
+        Intent intent = viewAction.actionIntent.getIntent();
+        assertNotNull(intent);
+        Bundle bundle = intent.getExtras();
+        assertTrue(bundle.containsKey(ScreenshotController.EXTRA_ID));
+        assertTrue(bundle.containsKey(ScreenshotController.EXTRA_SMART_ACTIONS_ENABLED));
+        assertEquals(ScreenshotController.ACTION_TYPE_VIEW, viewAction.title);
+        assertEquals(Intent.ACTION_VIEW, intent.getAction());
+    }
+
     // Tests for share action extras
     @Test
     public void testShareActionExtras() {